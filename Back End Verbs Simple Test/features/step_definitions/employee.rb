87ebbcc4ecd0ef0e90bc73de4bddf0ff24ddc671
AlexBianchini93 <77628701+AlexBianchini93@users.noreply.github.com>
2022-04-25 18:16:32 -0300

    Update employee.rb

--- Back End Verbs Simple Test/features/step_definitions/employee.rb
+++ Back End Verbs Simple Test/features/step_definitions/employee.rb
@@ -33,3 +33,27 @@ end
       expect(@create_employee['data']["employee_salary"]).to eql (420800)
       expect(@create_employee['data']["employee_age"]).to eql (30)
     end
+  
+  Dado('que o usuario altere uma informacao de funcionario') do
+    @put_url = 'http://dummy.restapiexample.com/api/v1/update/10'
+  end
+
+    Quando('ele eneviar as novas informacoes') do
+      @update_employee = HTTParty.put(@put_url, :headers => {'Content-Type': 'application/json'}, body: {
+        "employee_name": "Alberto",
+        "employee_salary": 100,
+        "employee_age": 40,
+        "profile_image": ""
+      }.to_json)
+    end
+    
+    Entao('as informacoes serao alterdas') do
+      expect(@update_employee.code).to eql (200)
+      expect(@update_employee.msg).to eql 'OK'
+      expect(@update_employee["status"]).to eql 'success'
+      expect(@update_employee["message"]).to eql 'Successfully! Record has been added.' 
+      expect(@update_employee['data']["employee_name"]).to eql 'Alberto'
+      expect(@update_employee['data']["employee_salary"]).to eql (100)
+      expect(@update_employee['data']["employee_age"]).to eql (40)
+    end
+    
